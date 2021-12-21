34575e410d2f11b507e8a9234d48661f2a4f3dde
Melissa <wolfaholic@users.noreply.github.com>
2021-12-20 19:19:42 -0800

    finished

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,11 +5,51 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+import mockFetchShow  from '../../api/fetchShow'
+
+const testShow = {
+    name:"Test",
+    summary:"Test Summary",
+    seasons: [
+        {
+            id: 0,
+            name: "Season 1",
+            episodes: []
+        },
+        {
+            id: 1,
+            name: "Season 2",
+            episodes: []
+        }
+    ],
+}
 
 test('renders without errors with no props', ()=>{
+    render(<Display />);
+});
+
+test('renders Show component when the button is clicked ', async ()=>{
+    mockFetchShow.mockresolvedValueOnce(testShow);
+
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
 
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders show season options matching your data when the button is clicked', async ()=>{
+    mockFetchShow.mockresolvedValueOnce(testShow);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+
+    })
+});
